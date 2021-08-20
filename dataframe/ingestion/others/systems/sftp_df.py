626d8c5e098e3d9a118d7da3efeaa6d41c25dab2
vaishali <vaishali9327@gmail.com>
2021-08-20 13:45:47 +0530

    emr

--- dataframe/ingestion/others/systems/sftp_df.py
+++ dataframe/ingestion/others/systems/sftp_df.py
@@ -33,4 +33,6 @@ if __name__ == '__main__':
 
     ol_txn_df.show(5, False)
 
+    ol_txn_df.write.mode('overwrite').parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/receipt_delta")
+
 # spark-submit --packages "com.springml:spark-sftp_2.11:1.1.1" dataframe/ingestion/others/systems/sftp_df.py
